c43b145d9809b18bad2e76e23fe0647c0e3ce2c5
Karol Wasemann <kw.kwaq@gmail.com>
2022-02-24 08:42:23 +0100

    task solved

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const btnTime = document.querySelector("th a");
-const arrow = btnTime.querySelector("i");
+const arrow = document.querySelector("i");
 const tableBody = document.querySelector("tbody");
 const rows = [...document.querySelectorAll("tbody tr")];
 
@@ -41,8 +41,9 @@ const timeFormat = (time) => {
 };
 
 const arrowDirection = (direction) => {
-  arrow.classList.remove(`fa-caret-${direction ? "down" : "up"}`);
-  arrow.classList.add(`fa-caret-${direction ? "up" : "down"}`);
+  const icon = document.querySelector("svg");
+  icon.classList.remove(`fa-caret-${direction ? "down" : "up"}`);
+  icon.classList.add(`fa-caret-${direction ? "up" : "down"}`);
 };
 
 const handleClick = () => {
